64fee4f42375015f9d191ab67574decaa3ce835c
KorbaLK <lukas-korber@web.de>
2015-03-31 17:19:45 +0200

    The next Change in my test Project

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
     System.out.println("Git is cool");
     System.out.println("Hello world1234");
     System.out.println("Hello world");
+    System.out.println("Hello world2");
+    System.out.println("Hello world3");
   }
 } 
