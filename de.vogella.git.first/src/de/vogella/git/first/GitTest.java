b08447d1b69b1d3529d4dd902301712ab652f1a7
KorbaLK <lukas-korber@web.de>
2015-03-31 16:52:38 +0200

    2nd Change

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -3,7 +3,7 @@ package de.vogella.git.first;
 public class GitTest {
   public static void main(String[] args) {
     System.out.println("Git is cool");
-    System.out.println("Hello world");
+    System.out.println("Hello world1234");
     System.out.println("Hello world");
   }
 } 
